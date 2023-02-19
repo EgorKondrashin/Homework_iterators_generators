4e31ecd987350901e704b29ab4588adf5a925e9e
Egor Kondrashin <kondrashin-egor@mail.ru>
2023-02-19 14:54:21 +0500

    Initial commit

---
+++ generator_my_list.py
@@ -0,0 +1,40 @@
+import types
+
+
+def flat_generator(list_of_lists):
+
+    my_list = list_of_lists
+    for new_list in my_list:
+        for i in range(len(new_list)):
+            yield new_list[i]
+
+
+list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
